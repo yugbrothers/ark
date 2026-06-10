f0c3b0b1d03a795c2bfa877248f5d50bdb04b71f
Abhishek Sharma <abhisheksharma1zac@gmail.com>
2026-06-10 15:08:40 +0530

    Convert ARK to Express web service

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`ARK running on port ${PORT}`);
+});
